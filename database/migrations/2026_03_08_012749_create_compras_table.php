1879b4823d31a614ed9ca0acaf35756999217925
unknown <rhoynnel.tur@gmail.com>
2026-03-07 21:52:56 -0400

    tablas de despacho y compras

---
+++ database/migrations/2026_03_08_012749_create_compras_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('compras', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('proveedors_id');
+            $table->string('numero_factura');
+            $table->date('fecha');
+            $table->boolean('estado')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('compras');
+    }
+};
---
+++ database/migrations/2026_03_08_012829_create_detalledespacho_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detalledespacho', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('despachos_id');
+            $table->foreignId('productos_id');
+            $table->integer('cantidad');
+            $table->decimal('precio_dolar', 10, 2);
+            $table->decimal('precio_bs', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detalledespacho');
+    }
+};
